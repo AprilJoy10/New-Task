467d20e17137838b3451fa09e093ca2a051c670a
AprilJoyReyes10 <reyesapriljoy.410@gmail.com>
2024-07-12 14:54:40 +0800

    Date time start

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="main">
+        <table class="tasks">
+            <thead>
+                <th>Task</th>
+                <th>Date time start</th>
+                <th>Date time end</th>
+                <th>Note</th>
+                <th>Created By</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> Created a dashboard</td>
+                    <td> 07/10/2024 2:00PM</td>
+                    <td> 07/12/2024 2:30PM</td>
+                    <td> Created a Table</td>
+                    <td> April</td>
+                </tr>
+                <tr>
+                    <td> Populated the table with sample data</td>
+                    <td> 07/10/2024 2:00PM</td>
+                    <td> 07/12/2024 2:30PM</td>
+                    <td> Created a Table</td>
+                    <td></td>
+                    <td> April</td>
+                </tr>
+                <tr>
+                    <td> Added task table</td>
+                    <td> 07/10/2024 2:00PM</td>
+                    <td> 07/12/2024 2:30PM</td>
+                    <td> Created a Table</td>
+                    <td></td>
+                    <td> April</td>
+                </tr>
+                <tr>
+                    <td> Added styles for task table</td>
+                    <td> 07/10/2024 3:00PM</td>
+                    <td> 07/12/2024 4:30PM</td>
+                    <td> Created a Table</td>
+                    <td></td>
+                    <td> April</td>
+                </tr>
+                <tr>
+                    <td> Added action for the table</td>
+                    <td> 07/10/2024 4:00PM</td>
+                    <td> 07/12/2024 5:00PM</td>
+                    <td> Created a Table</td>
+                    <td></td>
+                    <td> April</td>
+                </tr>
+                <tr>
+                    <td> Created a page for task form</td>
+                    <td> 07/10/2024 5:00PM</td>
+                    <td> 07/12/2024 6:00PM</td>
+                    <td> Created a Table</td>
+                    <td></td>
+                    <td> April</td>
+                </tr>
+
+            </tbody>
+        </table>      
+    </div>
+    
+</body>
+</html>
