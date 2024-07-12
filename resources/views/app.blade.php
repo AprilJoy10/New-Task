37b7d9df55985fb81e6b96850fa56cfe8ccf5c42
AprilJoyReyes10 <reyesapriljoy.410@gmail.com>
2024-07-12 15:47:14 +0800

    create layout structure

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body>
-    
+    @yield('content')
+    @stack('jsfiles')
 </body>
+
 </html>
